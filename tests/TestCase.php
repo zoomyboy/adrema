15faa13c51e478fb8fce231ec5056f7be2d3d059
philipp lang <philipp@aweos.de>
2021-06-13 11:30:31 +0200

    Add NamiFake to testcase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,18 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Zoomyboy\LaravelNami\Nami;
+use Zoomyboy\LaravelNami\FakesNami;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use FakesNami;
+
+    public function setUp(): void {
+        parent::setUp();
+    
+        $this->fakeNami();
+    }
+
 }
