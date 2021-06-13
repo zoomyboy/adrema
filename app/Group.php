5388ba71ffe1cec22170270604818d8d408ea331
philipp lang <philipp@aweos.de>
2021-06-13 11:27:22 +0200

    Add GroupFactory

--- app/Group.php
+++ app/Group.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Group extends Model
 {
+    use HasFactory;
+
     public $fillable = ['name', 'nami_id'];
 
     public $timestamps = false;
