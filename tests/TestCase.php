700de57f69adba918593eb72a4ce69b520013f11
philipp lang <philipp@aweos.de>
2021-07-17 15:56:37 +0200

    Add lastname for faked logged in member

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-use Zoomyboy\LaravelNami\Nami;
 use Zoomyboy\LaravelNami\FakesNami;
+use Zoomyboy\LaravelNami\Nami;
 use Zoomyboy\LaravelNami\NamiUser;
 
 abstract class TestCase extends BaseTestCase
@@ -20,7 +20,7 @@ abstract class TestCase extends BaseTestCase
 
     public function login() {
         $this->fakeNamiMembers([
-            [ 'gruppierungId' => 12399, 'vorname' => 'Max', 'id' => 999 ]
+            [ 'gruppierungId' => 12399, 'vorname' => 'Max', 'nachname' => '::lastname::', 'id' => 999 ]
         ]);
 
         $this->fakeNamiPassword(999, 'secret', [12399]);
