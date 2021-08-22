9662df4c7c45b5404a2107aa41dde851567957a3
philipp lang <philipp@aweos.de>
2021-08-22 22:53:43 +0200

    Add membership create

--- app/Membership/MembershipController.php
+++ app/Membership/MembershipController.php
@@ -2,8 +2,10 @@
 
 namespace App\Membership;
 
+use App\Activity;
 use App\Http\Controllers\Controller;
 use App\Member\Member;
+use App\Subactivity;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
@@ -11,7 +13,18 @@ class MembershipController extends Controller
 {
     public function store(Member $member, Request $request): RedirectResponse
     {
-        $member->memberships()->create($request->input());
+        $namiId = auth()->user()->api()->group($member->group->nami_id)->member($member->nami_id)
+            ->putMembership([
+                'created_at' => now(),
+                'group_id' => $member->group->nami_id,
+                'activity_id' => Activity::find($request->input('activity_id'))->nami_id,
+                'subactivity_id' => optional(Subactivity::find($request->input('subactivity_id')))->nami_id,
+            ]);
+
+        $membership = $member->memberships()->create(array_merge(
+            $request->input(),
+            ['nami_id' => $namiId],
+        ));
 
         return redirect()->back();
     }
