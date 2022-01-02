d0fa494011e3b2939f5e604fd5589af89c9e3a68
philipp lang <philipp@aweos.de>
2022-01-02 12:43:06 +0100

    Add nami provider to login screen

--- resources/js/views/Login.vue
+++ resources/js/views/Login.vue
@@ -20,7 +20,9 @@ export default {
 
     data: function() {
         return {
-            values: {}
+            values: {
+                provider: 'nami',
+            }
         };
     },
     methods: {
