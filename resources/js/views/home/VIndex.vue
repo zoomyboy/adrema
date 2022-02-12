43077016a6e7d01392649a0ce3b3b1df39d6bb0c
philipp lang <philipp@aweos.de>
2022-02-12 14:56:25 +0100

    Rename v-block

--- resources/js/views/home/VIndex.vue
+++ resources/js/views/home/VIndex.vue
@@ -1,40 +1,41 @@
 <template>
     <div class="gap-6 grid-cols-4 grid p-6">
-        <block title="Ausstehende Mitgliedsbeiträge">
+        <v-block title="Ausstehende Mitgliedsbeiträge">
             <div class="text-gray-100">
                 <span class="text-xl mr-1 font-semibold" v-text="data.payments.amount"></span>
                 <span class="text-sm" v-text="`von ${data.payments.users} / ${data.payments.all_users} Mitgliedern`"></span>
             </div>
-        </block>
-        <block title="Gruppierungs-Verteilung">
+        </v-block>
+        <v-block title="Gruppierungs-Verteilung">
             <div v-for="group, index in data.groups" :key="index" class="flex mt-2 items-center leading-none text-gray-100">
                 <sprite class="w-4 h-4 mr-2" src="lilie" :class="`text-${group.slug}`"></sprite>
                 <span v-text="group.name" class="grow"></span>
                 <span v-text="group.count"></span>
             </div>
-        </block>
-        <block title="Endende Schhnupperzeiten">
+        </v-block>
+        <v-block title="Endende Schhnupperzeiten">
             <div v-for="member, index in data.ending_tries" :key="index" class="flex mt-2 items-center leading-none text-gray-100">
                 <span class="grow" v-text="`${member.firstname} ${member.lastname}`"></span>
                 <span class="mr-2 text-sm tex-gray-600" v-text="`${member.try_ends_at}`"></span>
                 <span class="text-xs tex-gray-600" v-text="`${member.try_ends_at_human}`"></span>
             </div>
-        </block>
+        </v-block>
     </div>
 </template>
 
 <script>
 import App from '../../layouts/App';
-import Block from './Block.vue';
 
 export default {
     layout: App,
 
-    components: { Block },
-
     props: {
         data: {},
         blocks: {}
+    },
+
+    components: {
+        'VBlock': () => import('./VBlock')
     }
 
 };
