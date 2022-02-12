cdb4d2863bc0ec8776de6e2a627a5cc9bd0cb0b5
philipp lang <philipp@aweos.de>
2022-02-12 14:52:37 +0100

    Fix prop mutation

--- resources/js/views/member/Filt.vue
+++ resources/js/views/member/Filt.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="px-6 py-2 flex border-b border-gray-600 space-x-3">
-        <f-switch v-show="hasModule('bill')" id="ausstand" @input="reload" v-model="value.ausstand" label="Nur Ausstände" size="sm"></f-switch>
-        <f-select v-show="hasModule('bill')" id="billKinds" @input="reload" :options="billKinds" v-model="value.bill_kind" label="Rechnung" size="sm"></f-select>
-        <f-select id="activity_id" @input="reload" :options="activities" v-model="value.activity_id" label="Tätigkeit" size="sm"></f-select>
-        <f-select id="subactivity_id" @input="reload" :options="subactivities" v-model="value.subactivity_id" label="Untertätigkeit" size="sm"></f-select>
+        <f-switch v-show="hasModule('bill')" id="ausstand" @input="reload" v-model="inner.ausstand" label="Nur Ausstände" size="sm"></f-switch>
+        <f-select v-show="hasModule('bill')" id="billKinds" @input="reload" :options="billKinds" v-model="inner.bill_kind" label="Rechnung" size="sm"></f-select>
+        <f-select id="activity_id" @input="reload" :options="activities" v-model="inner.activity_id" label="Tätigkeit" size="sm"></f-select>
+        <f-select id="subactivity_id" @input="reload" :options="subactivities" v-model="inner.subactivity_id" label="Untertätigkeit" size="sm"></f-select>
     </div>
 </template>
 
@@ -11,6 +11,12 @@
 import mergesQueryString from '../../mixins/mergesQueryString.js';
 
 export default {
+
+    data: function() {
+        return {
+            inner: {}
+        };
+    },
 
     mixins: [mergesQueryString],
 
@@ -23,10 +29,15 @@ export default {
 
     methods: {
         reload() {
-            this.$inertia.visit(this.qs({filter: JSON.stringify(this.value)}), {
+            this.$inertia.visit(this.qs({filter: JSON.stringify(this.inner)}), {
                 preserveState: true
             });
         }
+    },
+
+    created() {
+        this.inner = this.value;
     }
+
 };
 </script>
