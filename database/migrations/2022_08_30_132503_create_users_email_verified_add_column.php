7dc69015f81b9a07359f583fcc3b141851bddc56
Philipp Lang <philipp@aweos.de>
2022-08-30 15:27:58 +0200

    Create user columns

---
+++ database/migrations/2022_08_30_132503_create_users_email_verified_add_column.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('email')->unique()->change();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->rememberToken();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+        });
+    }
+};
