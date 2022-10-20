2189d95da8a5a9f782b17e3d47525e717dc56fc4
philipp lang <philipp@aweos.de>
2022-10-20 02:25:56 +0200

    Add introduction

--- resources/js/views/setting/Mailman.vue
+++ resources/js/views/setting/Mailman.vue
@@ -1,5 +1,15 @@
 <template>
     <form class="grow p-6 grid grid-cols-2 gap-3 items-start content-start" @submit.prevent="submit">
+        <div class="col-span-full text-gray-100 mb-3">
+            <p class="text-sm">
+                Scoutrobot kann automatisch Mailinglisten erstellen, wenn es mit einem existierenden
+                <a href="https://docs.mailman3.org/en/latest/">Mailman Server</a> verbunden wird. Mailman ist ein
+                OpenSource-Mailinglisten-System, um E-Mails an mehrere Leute zu senden.
+            </p>
+            <p class="text-sm mt-1">
+                Scoutrobot wird nach der Ersteinrichtung deine Mitglieder zu bestehenden E-Mail-Verteilern hinzufügen.
+            </p>
+        </div>
         <f-text label="URL" hint="URL der Mailman Api" name="base_url" id="base_url" v-model="inner.base_url"></f-text>
         <f-text label="Benutzername" name="username" id="username" v-model="inner.username"></f-text>
         <f-text label="Passwort" name="password" id="password" v-model="inner.password"></f-text>
