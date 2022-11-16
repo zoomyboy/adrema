2999342c1a0f4b0efb1bffd988c5f166f62a3fd3
philipp lang <philipp@aweos.de>
2022-11-16 23:24:37 +0100

    fix memberView

--- app/Http/Views/MemberView.php
+++ app/Http/Views/MemberView.php
@@ -21,8 +21,8 @@ class MemberView
         return [
             'data' => MemberResource::collection(Member::select('*')
                 ->filter($filter)->search($request->query('search', null))
-                ->with('billKind')->with('payments')->with('memberships')->with('courses')
-                ->withSubscriptionName()->withIsConfirmed()->withPendingPayment()->withAgeGroup()->withIsLeader()
+                ->with('billKind')->with('payments')->with('memberships')->with('courses')->with('leaderMemberships')->with('ageGroupMemberships')
+                ->withSubscriptionName()->withIsConfirmed()->withPendingPayment()
                 ->orderByRaw('lastname, firstname')
                 ->paginate(15)
             ),
