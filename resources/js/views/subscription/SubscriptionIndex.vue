a16c0f469c9340e055f2faa98957a3b29902ad45
philipp lang <philipp@aweos.de>
2022-11-17 00:05:40 +0100

    Fixed Subscription index

--- resources/js/views/subscription/SubscriptionIndex.vue
+++ resources/js/views/subscription/SubscriptionIndex.vue
@@ -1,31 +1,37 @@
 <template>
     <div>
-        <div class="custom-table">
-            <header>
-                <div class="px-6 text-gray-200 font-semibold py-3 border-gray-600 border-b">Name</div>
-                <div class="px-6 text-gray-200 font-semibold py-3 border-gray-600 border-b">Interner Beitrag</div>
-                <div class="px-6 text-gray-200 font-semibold py-3 border-gray-600 border-b">Nami-Beitrag</div>
-                <div class="px-6 py-3 border-gray-600 border-b"></div>
-            </header>
+        <table cellspacing="0" cellpadding="0" border="0" class="custom-table custom-table-sm">
+            <thead>
+                <th>Name</th>
+                <th>Interner Beitrag</th>
+                <th>Nami-Beitrag</th>
+                <th></th>
+            </thead>
 
-            <div v-for="sub, index in data" class="text-gray-200 transition-all duration-300 rounded flex items-center hover:bg-gray-800" :key="index">
-                <div class="py-1 px-6" v-text="sub.name"></div>
-                <div class="py-1 px-6" v-text="sub.amount_human"></div>
-                <div class="py-1 px-6" v-text="sub.fee_name"></div>
-                <div class="py-1 px-6 flex">
-                    <i-link :href="`/subscription/${sub.id}/edit`" class="inline-flex btn btn-warning btn-sm"><svg-sprite src="pencil"></svg-sprite></i-link>
-                </div>
-            </div>
-
-        </div>
-
+            <tr v-for="(subscription, index) in data" :key="index">
+                <td>
+                    <div v-text="subscription.name"></div>
+                </td>
+                <td>
+                    <div v-text="subscription.amount_human"></div>
+                </td>
+                <td>
+                    <div v-text="subscription.fee_name"></div>
+                </td>
+                <td>
+                    <i-link :href="`/subscription/${subscription.id}/edit`" class="inline-flex btn btn-warning btn-sm"
+                        ><svg-sprite src="pencil"></svg-sprite
+                    ></i-link>
+                </td>
+            </tr>
+        </table>
     </div>
 </template>
 
 <script>
 export default {
-    props:{
-        data: {}
-    }
-}
+    props: {
+        data: {},
+    },
+};
 </script>
