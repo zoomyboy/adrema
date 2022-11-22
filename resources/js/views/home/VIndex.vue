5a70d341647c5a26b95cd003563bf8e4c0e14460
philipp lang <philipp@aweos.de>
2022-11-23 00:46:20 +0100

    fix home layout

--- resources/js/views/home/VIndex.vue
+++ resources/js/views/home/VIndex.vue
@@ -1,16 +1,8 @@
 <template>
-    <div class="gap-6 grid-cols-4 grid p-6">
+    <div class="gap-6 md:grid-cols-2 xl:grid-cols-4 grid p-6">
         <v-block v-for="(block, index) in blocks" :key="index" :title="block.title">
             <v-component :data="block.data" :is="block.component"></v-component>
         </v-block>
-        <!--
-        <v-block title="Ausstehende Mitgliedsbeiträge">
-        </v-block>
-        <v-block title="Gruppierungs-Verteilung">
-        </v-block>
-        <v-block title="Endende Schhnupperzeiten">
-        </v-block>
-        -->
     </div>
 </template>
 
