55e5ae18099363a4959668e1c36d96c13ffb495d
philipp lang <philipp@aweos.de>
2022-12-04 23:54:07 +0100

    fix login screen

--- resources/js/layouts/FullLayout.vue
+++ resources/js/layouts/FullLayout.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="app" class="bg-gray-900 font-sans flex flex-col grow items-center justify-center">
         <v-notification class="fixed z-40 right-0 bottom-0 mb-3 mr-3"></v-notification>
-        <div class="w-96 bg-gray-800 rounded-xl overflow-hidden shadow-lg">
+        <div class="w-64 sm:w-72 md:w-96 bg-gray-800 rounded-xl overflow-hidden shadow-lg">
             <slot></slot>
         </div>
     </div>
@@ -10,7 +10,7 @@
 <script>
 export default {
     components: {
-        VNotification: () => import('../components/VNotification.vue')
+        VNotification: () => import('../components/VNotification.vue'),
     },
 };
 </script>
--- resources/js/views/VLogin.vue
+++ resources/js/views/VLogin.vue
@@ -1,10 +1,10 @@
 <template>
     <form @submit.prevent="submit">
-        <div class="h-24 px-10 bg-primary-800 flex justify-between items-center w-full">
+        <div class="h-24 p-6 md:px-10 bg-primary-800 flex justify-between items-center w-full">
             <span class="text-primary-500 text-xl">Login</span>
-            <img src="/img/dpsg.gif" class="w-24">
+            <img src="/img/dpsg.gif" class="w-24" />
         </div>
-        <div class="p-10 grid gap-5">
+        <div class="p-6 md:p-10 grid gap-5">
             <f-text id="email" label="E-Mail-Adresse" v-model="values.email"></f-text>
             <f-text id="password" type="password" label="Passwort" v-model="values.password"></f-text>
             <button type="submit" class="btn btn-primary">Login</button>
@@ -18,18 +18,18 @@ import FullLayout from '../layouts/FullLayout';
 export default {
     layout: FullLayout,
 
-    data: function() {
+    data: function () {
         return {
             values: {
                 email: '',
                 password: '',
-            }
+            },
         };
     },
     methods: {
         submit() {
             this.$inertia.post('/login', this.values);
-        }
-    }
+        },
+    },
 };
 </script>
