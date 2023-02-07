311f1281f6905ec3a4c3c0b96ffe2a32e8c6f3e3
philipp lang <philipp@aweos.de>
2023-02-07 23:42:49 +0100

    Fix tests

--- tests/Feature/Initialize/InitializeMembersTest.php
+++ tests/Feature/Initialize/InitializeMembersTest.php
@@ -6,6 +6,7 @@ use App\Actions\PullCoursesAction;
 use App\Actions\PullMemberAction;
 use App\Actions\PullMembershipsAction;
 use App\Initialize\InitializeMembers;
+use App\Member\Member;
 use App\Setting\NamiSettings;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
@@ -19,13 +20,14 @@ class InitializeMembersTest extends TestCase
     public function testItInitializesMembers(): void
     {
         $this->loginNami();
+        $member = Member::factory()->defaults()->create();
         $api = app(NamiSettings::class)->login();
         app(SearchFake::class)->fetches(1, 0, [
             MemberEntry::factory()->toMember(['groupId' => 100, 'id' => 20]),
         ]);
-        PullMemberAction::shouldRun()->once()->with(100, 20);
-        PullMembershipsAction::shouldRun()->once();
-        PullCoursesAction::shouldRun()->once();
+        PullMemberAction::shouldRun()->once()->with(100, 20)->andReturn($member);
+        PullMembershipsAction::shouldRun()->once()->with($member);
+        PullCoursesAction::shouldRun()->once()->with($member);
 
         app(InitializeMembers::class)->handle($api);
     }
