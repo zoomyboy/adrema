197c23354d3cc5cbcaac43900315a4185128f2cf
philipp lang <philipp@aweos.de>
2023-03-04 14:47:37 +0100

    fix: select subactivity

--- resources/js/views/activity/NewSubactivity.vue
+++ resources/js/views/activity/NewSubactivity.vue
@@ -5,7 +5,7 @@
         <div class="mt-2 border border-primary-700 rounded-lg p-5" v-if="model !== null">
             <div class="flex space-x-3">
                 <f-text size="sm" id="name" v-model="model.name" label="Name" required></f-text>
-                <f-switch size="sm" v-model="model.is_filterable" name="is_filterable" id="is_filterable" label="Filterbar"></f-switch>
+                <f-switch size="sm" v-model="model.is_filterable" name="subactivity_is_filterable" id="subactivity_is_filterable" label="Filterbar"></f-switch>
             </div>
             <icon-button class="mt-3" icon="save" @click.prevent="store">Speichern</icon-button>
         </div>
