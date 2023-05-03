ff637a5d88d7ef3d60b0a4434a6aebaa9d6e590c
philipp lang <philipp@aweos.de>
2023-05-03 22:36:02 +0200

    Add tries for member job

--- app/Nami/Api/FullMemberAction.php
+++ app/Nami/Api/FullMemberAction.php
@@ -5,6 +5,7 @@ namespace App\Nami\Api;
 use App\Member\Data\FullMember;
 use Illuminate\Support\Facades\Redis;
 use Lorisleiva\Actions\Concerns\AsAction;
+use Lorisleiva\Actions\Decorators\JobDecorator;
 use Zoomyboy\LaravelNami\Api;
 
 class FullMemberAction
@@ -24,5 +25,10 @@ class FullMemberAction
         }
 
         return $fullMember;
+    }
+
+    public function configureJob(JobDecorator $job): void
+    {
+        $job->setTries(8);
     }
 }
