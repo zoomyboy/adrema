fbb501519ab07a5191b67dddd75fe434498efa17
Philipp Lang <privat@zoomyboy.de>
2023-06-01 11:05:13 +0200

    add vite script

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,7 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
         <meta name="socketport" content="{{env('SOCKET_PORT')}}" />
-        <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
-        <script src="{{ mix('/js/app.js') }}" defer></script>
+        @vite('resources/js/app.js')
     </head>
     <body class="min-h-full flex flex-col">
         @inertia
