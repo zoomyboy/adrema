71e6b0216f6112bebe0cc11f28b530bc67f71f81
Philipp Lang <privat@zoomyboy.de>
2023-07-06 16:53:33 +0200

    Lint

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -32,7 +32,7 @@ class HandleInertiaRequests extends Middleware
     /**
      * Defines the props that are shared by default.
      *
-     * @see https://inertiajs.com/shared-data
+     * @return array<string, mixed>
      */
     public function share(Request $request): array
     {
