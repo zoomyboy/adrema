1583e77756d4cba38164b6bdcf834b80194d4514
Philipp Lang <privat@zoomyboy.de>
2023-07-24 18:19:32 +0200

    Add schedule container

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,6 +28,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command(PruneCommand::class, ['--hours' => 168])->daily();     // 168h = 7 Tage
+        $schedule->command(InitializeMembers::class)->dailyAt('03:00');
     }
 
     /**
