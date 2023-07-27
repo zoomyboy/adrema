59c2c527fb1c8c960196ed9b0cca4cec731c6684
Arwed Molitor <arwed@dpsg-niederrad.de>
2023-07-27 14:06:28 +0200

    Change tag order

--- resources/js/views/member/Tags.vue
+++ resources/js/views/member/Tags.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="bool-row">
-        <ui-bool true-comment="Mittendrin abonniert" false-comment="Mittendrin nicht abonníert" v-model="member.send_newspaper">M</ui-bool>
         <ui-bool true-comment="In NaMi eingetragen" false-comment="Nicht in NaMi eingetragen" v-model="member.has_nami">N</ui-bool>
+        <ui-bool true-comment="Mittendrin abonniert" false-comment="Mittendrin nicht abonníert" v-model="member.send_newspaper">M</ui-bool>
     </div>
 </template>
 
