6d4dda869a7d6d6fdda8ce8e67a546b2845892ca
Philipp Lang <privat@zoomyboy.de>
2023-10-16 16:39:06 +0200

    Add DestroyAction for payment

--- app/Payment/Actions/PaymentDestroyAction.php
+++ app/Payment/Actions/PaymentDestroyAction.php
@@ -2,36 +2,41 @@
 
 namespace App\Payment\Actions;
 
+use App\Lib\JobMiddleware\JobChannels;
+use App\Lib\JobMiddleware\WithJobState;
+use App\Lib\Queue\TracksJob;
+use App\Payment\Payment;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Validation\Rules\In;
 use Lorisleiva\Actions\Concerns\AsAction;
 
 class PaymentDestroyAction
 {
     use AsAction;
+    use TracksJob;
 
-    public function handle(): void
+    public function handle(Payment $payment): void
     {
+        $payment->delete();
     }
 
-    /**
-     * @return array<string, array<int, string|In>>
-     */
-    public function rules(): array
+    public function asController(Payment $payment): JsonResponse
     {
-        return [];
-    }
+        $this->startJob($payment);
 
-    /**
-     * @return array<string, string>
-     */
-    public function getValidationAttributes(): array
-    {
-        return [];
-    }
-
-    public function asController(): JsonResponse
-    {
         return response()->json([]);
+    }
+
+    /**
+     * @param mixed $parameters
+     */
+    public function jobState(WithJobState $jobState, ...$parameters): WithJobState
+    {
+        $member = $parameters[0]->member;
+
+        return $jobState
+            ->before('Zahlung für ' . $member->fullname . ' wird gelöscht')
+            ->after('Zahlung für ' . $member->fullname . ' gelöscht')
+            ->failed('Fehler beim Löschen der Zahlung für ' . $member->fullname)
+            ->shouldReload(JobChannels::make()->add('member')->add('payment'));
     }
 }
