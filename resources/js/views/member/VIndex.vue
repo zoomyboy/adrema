1e836b2a28169114d351ec6a20f00ef5309bc02f
Philipp Lang <privat@zoomyboy.de>
2023-11-16 12:10:07 +0100

    Fix bill table

--- resources/js/views/member/VIndex.vue
+++ resources/js/views/member/VIndex.vue
@@ -1,23 +1,17 @@
 <template>
     <page-layout page-class="pb-6">
         <template #toolbar>
-            <page-toolbar-button :href="meta.links.create" color="primary" icon="plus">Mitglied
-                anlegen</page-toolbar-button>
-            <page-toolbar-button v-if="hasModule('bill')" :href="meta.links.allpayment" color="primary"
-                icon="invoice">Rechnungen erstellen</page-toolbar-button>
-            <page-toolbar-button v-if="hasModule('bill')" :href="meta.links.sendpayment" color="info"
-                icon="envelope">Rechnungen versenden</page-toolbar-button>
+            <page-toolbar-button :href="meta.links.create" color="primary" icon="plus">Mitglied anlegen</page-toolbar-button>
+            <page-toolbar-button v-if="hasModule('bill')" :href="meta.links.allpayment" color="primary" icon="invoice">Rechnungen erstellen</page-toolbar-button>
+            <page-toolbar-button v-if="hasModule('bill')" :href="meta.links.sendpayment" color="info" icon="envelope">Rechnungen versenden</page-toolbar-button>
         </template>
         <ui-popup v-if="deleting !== null" heading="Mitglied löschen?" @close="deleting.reject()">
             <div>
                 <p class="mt-4">Das Mitglied "{{ deleting.member.fullname }}" löschen?</p>
-                <p class="mt-2">Alle Zuordnungen (Ausbildungen, Rechnungen, Zahlungen, Tätigkeiten) werden ebenfalls
-                    entfernt.</p>
-                <ui-note v-if="!deleting.member.has_nami" class="mt-5" type="warning"> Dieses Mitglied ist nicht in NaMi
-                    vorhanden und wird daher nur in der AdReMa gelöscht werden. </ui-note>
+                <p class="mt-2">Alle Zuordnungen (Ausbildungen, Rechnungen, Zahlungen, Tätigkeiten) werden ebenfalls entfernt.</p>
+                <ui-note v-if="!deleting.member.has_nami" class="mt-5" type="warning"> Dieses Mitglied ist nicht in NaMi vorhanden und wird daher nur in der AdReMa gelöscht werden. </ui-note>
                 <ui-note v-if="deleting.member.has_nami" class="mt-5" type="danger">
-                    Dieses Mitglied ist in NaMi vorhanden und wird daher in NaMi abgemeldet werden. Sofern
-                    "Datenweiterverwendung" eingeschaltet ist, wird das Mitglied auf inaktiv gesetzt.
+                    Dieses Mitglied ist in NaMi vorhanden und wird daher in NaMi abgemeldet werden. Sofern "Datenweiterverwendung" eingeschaltet ist, wird das Mitglied auf inaktiv gesetzt.
                 </ui-note>
                 <div class="grid grid-cols-2 gap-3 mt-6">
                     <a href="#" class="text-center btn btn-danger" @click.prevent="deleting.resolve">Mitglied loschen</a>
@@ -26,22 +20,45 @@
             </div>
         </ui-popup>
         <page-filter breakpoint="xl">
-            <f-text id="search" :model-value="getFilter('search')" name="search" label="Suchen …" size="sm"
-                @update:model-value="setFilter('search', $event)"></f-text>
-            <f-switch v-show="hasModule('bill')" id="ausstand" :model-value="getFilter('ausstand')" label="Nur Ausstände"
-                size="sm" @update:model-value="setFilter('ausstand', $event)"></f-switch>
-            <f-multipleselect id="group_ids" :options="meta.groups" :model-value="getFilter('group_ids')"
-                label="Gruppierungen" size="sm" name="group_ids"
-                @update:model-value="setFilter('group_ids', $event)"></f-multipleselect>
-            <f-select v-show="hasModule('bill')" id="billKinds" name="billKinds" :options="meta.billKinds"
-                :model-value="getFilter('bill_kind')" label="Rechnung" size="sm"
-                @update:model-value="setFilter('bill_kind', $event)"></f-select>
-            <f-multipleselect id="activity_ids" :options="meta.filterActivities" :model-value="getFilter('activity_ids')"
-                label="Tätigkeiten" size="sm" name="activity_ids"
-                @update:model-value="setFilter('activity_ids', $event)"></f-multipleselect>
-            <f-multipleselect id="subactivity_ids" :options="meta.filterSubactivities"
-                :model-value="getFilter('subactivity_ids')" label="Untertätigkeiten" size="sm" name="subactivity_ids"
-                @update:model-value="setFilter('subactivity_ids', $event)"></f-multipleselect>
+            <f-text id="search" :model-value="getFilter('search')" name="search" label="Suchen …" size="sm" @update:model-value="setFilter('search', $event)"></f-text>
+            <f-switch v-show="hasModule('bill')" id="ausstand" :model-value="getFilter('ausstand')" label="Nur Ausstände" size="sm" @update:model-value="setFilter('ausstand', $event)"></f-switch>
+            <f-multipleselect
+                id="group_ids"
+                :options="meta.groups"
+                :model-value="getFilter('group_ids')"
+                label="Gruppierungen"
+                size="sm"
+                name="group_ids"
+                @update:model-value="setFilter('group_ids', $event)"
+            ></f-multipleselect>
+            <f-select
+                v-show="hasModule('bill')"
+                id="billKinds"
+                name="billKinds"
+                :options="meta.billKinds"
+                :model-value="getFilter('bill_kind')"
+                label="Rechnung"
+                size="sm"
+                @update:model-value="setFilter('bill_kind', $event)"
+            ></f-select>
+            <f-multipleselect
+                id="activity_ids"
+                :options="meta.filterActivities"
+                :model-value="getFilter('activity_ids')"
+                label="Tätigkeiten"
+                size="sm"
+                name="activity_ids"
+                @update:model-value="setFilter('activity_ids', $event)"
+            ></f-multipleselect>
+            <f-multipleselect
+                id="subactivity_ids"
+                :options="meta.filterSubactivities"
+                :model-value="getFilter('subactivity_ids')"
+                label="Untertätigkeiten"
+                size="sm"
+                name="subactivity_ids"
+                @update:model-value="setFilter('subactivity_ids', $event)"
+            ></f-multipleselect>
             <button class="btn btn-primary label mr-2" @click.prevent="exportMembers">
                 <ui-sprite class="w-3 h-3 xl:mr-2" src="save"></ui-sprite>
                 <span class="hidden xl:inline">Exportieren</span>
@@ -56,8 +73,8 @@
                 <th class="!hidden 2xl:!table-cell">Ort</th>
                 <th>Tags</th>
                 <th class="!hidden xl:!table-cell">Alter</th>
-                <th v-show="hasModule('bill')" class="!hidden xl:!table-cell">Rechnung</th>
-                <th v-show="hasModule('bill')">Ausstand</th>
+                <th v-if="hasModule('bill')" class="!hidden xl:!table-cell">Rechnung</th>
+                <th v-if="hasModule('bill')">Ausstand</th>
                 <th></th>
             </thead>
 
@@ -70,10 +87,10 @@
                     <tags :member="member"></tags>
                 </td>
                 <td class="!hidden xl:!table-cell" v-text="member.age"></td>
-                <td v-show="hasModule('bill')" class="!hidden xl:!table-cell">
+                <td v-if="hasModule('bill')" class="!hidden xl:!table-cell">
                     <ui-label :value="member.bill_kind_name" fallback="kein"></ui-label>
                 </td>
-                <td v-show="hasModule('bill')">
+                <td v-if="hasModule('bill')">
                     <ui-label :value="member.pending_payment" fallback="---"></ui-label>
                 </td>
                 <td>
@@ -90,14 +107,11 @@
                 <div class="text-xs text-gray-200" v-text="member.full_address"></div>
                 <div class="flex items-center mt-1 space-x-4">
                     <tags :member="member"></tags>
-                    <ui-label v-show="hasModule('bill')" class="text-gray-100 block" :value="member.pending_payment"
-                        fallback=""></ui-label>
+                    <ui-label v-show="hasModule('bill')" class="text-gray-100 block" :value="member.pending_payment" fallback=""></ui-label>
                 </div>
-                <actions class="mt-2" :member="member" @sidebar="openSidebar($event, member)" @remove="remove(member)">
-                </actions>
+                <actions class="mt-2" :member="member" @sidebar="openSidebar($event, member)" @remove="remove(member)"> </actions>
                 <div class="absolute right-0 top-0 h-full flex items-center mr-2">
-                    <i-link v-tooltip="`Details`" :href="member.links.show"><ui-sprite src="chevron"
-                            class="w-6 h-6 text-teal-100 -rotate-90"></ui-sprite></i-link>
+                    <i-link v-tooltip="`Details`" :href="member.links.show"><ui-sprite src="chevron" class="w-6 h-6 text-teal-100 -rotate-90"></ui-sprite></i-link>
                 </div>
             </ui-box>
         </div>
@@ -107,12 +121,9 @@
         </div>
 
         <ui-sidebar v-if="single !== null" @close="closeSidebar">
-            <member-payments v-if="single.type === 'payment'" :url="single.model.links.payment_index"
-                @close="closeSidebar"></member-payments>
-            <member-memberships v-if="single.type === 'membership'" :url="single.model.links.membership_index"
-                @close="closeSidebar"></member-memberships>
-            <member-courses v-if="single.type === 'courses'" :url="single.model.links.course_index"
-                @close="closeSidebar"></member-courses>
+            <member-payments v-if="single.type === 'payment'" :url="single.model.links.payment_index" @close="closeSidebar"></member-payments>
+            <member-memberships v-if="single.type === 'membership'" :url="single.model.links.membership_index" @close="closeSidebar"></member-memberships>
+            <member-courses v-if="single.type === 'courses'" :url="single.model.links.course_index" @close="closeSidebar"></member-courses>
         </ui-sidebar>
     </page-layout>
 </template>
@@ -123,14 +134,14 @@ import MemberMemberships from './MemberMemberships.vue';
 import MemberCourses from './MemberCourses.vue';
 import Tags from './Tags.vue';
 import Actions from './index/Actions.vue';
-import { indexProps, useIndex } from '../../composables/useIndex.js';
-import { ref, defineProps } from 'vue';
+import {indexProps, useIndex} from '../../composables/useIndex.js';
+import {ref, defineProps} from 'vue';
 
 const single = ref(null);
 const deleting = ref(null);
 
 const props = defineProps(indexProps);
-var { router, data, meta, getFilter, setFilter, filterString, reloadPage } = useIndex(props.data, 'member');
+var {router, data, meta, getFilter, setFilter, filterString, reloadPage} = useIndex(props.data, 'member');
 
 function exportMembers() {
     window.open(`/member-export?filter=${filterString.value}`);
@@ -138,7 +149,7 @@ function exportMembers() {
 
 async function remove(member) {
     new Promise((resolve, reject) => {
-        deleting.value = { resolve, reject, member };
+        deleting.value = {resolve, reject, member};
     })
         .then(() => {
             router.delete(`/member/${member.id}`);
