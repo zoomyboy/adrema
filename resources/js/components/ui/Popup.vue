02cd70e0cae6f8b72a86e39a9fe3bf4c2dd5f82e
Philipp Lang <privat@zoomyboy.de>
2023-12-16 22:59:25 +0100

    Add innerWidth for Popup component

--- resources/js/components/ui/Popup.vue
+++ resources/js/components/ui/Popup.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="fixed z-40 top-0 left-0 w-full h-full flex items-center justify-center p-6">
-        <div class="relative rounded-lg p-8 bg-zinc-800 shadow-2xl shadow-black border border-zinc-700 border-solid w-full max-w-xl">
+        <div class="relative rounded-lg p-8 bg-zinc-800 shadow-2xl shadow-black border border-zinc-700 border-solid w-full" :class="innerWidth">
             <a href="#" class="absolute top-0 right-0 mt-6 mr-6" @click.prevent="$emit('close')">
                 <ui-sprite src="close" class="text-zinc-400 w-6 h-6"></ui-sprite>
             </a>
-            <h3 class="font-semibold text-primary-200 text-xl" v-html="heading"></h3>
+            <h3 v-if="heading" class="font-semibold text-primary-200 text-xl" v-html="heading"></h3>
             <div class="text-primary-100">
                 <slot></slot>
             </div>
@@ -15,7 +15,14 @@
 <script>
 export default {
     props: {
-        heading: {},
+        heading: {
+            type: String,
+            default: () => '',
+        },
+        innerWidth: {
+            default: () => 'max-w-xl',
+            type: String,
+        },
     },
 };
 </script>
