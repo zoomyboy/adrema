edb1eeb0d885b25de8f06069487ed759a36d7178
philipp lang <philipp@aweos.de>
2023-12-25 18:38:32 +0100

    Add path alias for packages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -24,4 +25,9 @@ export default defineConfig({
             },
         }),
     ],
+    resolve: {
+        alias: {
+            '!': path.resolve(__dirname, './packages'),
+        },
+    },
 });
