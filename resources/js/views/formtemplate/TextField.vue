0eb8928b22864f36160c4cb52eeb0a5e8ac8790b
philipp lang <philipp@aweos.de>
2023-12-27 23:25:34 +0100

    Fix TextField Meta

--- resources/js/views/formtemplate/TextField.vue
+++ resources/js/views/formtemplate/TextField.vue
@@ -1,20 +1,11 @@
 <template>
-    <f-switch
-        id="fieldrequired"
-        v-model="modelValue.required"
-        label="Erforderlich"
-        size="sm"
-        name="fieldrequired"
-        inline
-        @update:modelValue="$emit('update:modelValue', {...modelValue, required: $event})"
-    ></f-switch>
+    <f-switch id="fieldrequired" :model-value="modelValue.required" label="Erforderlich" size="sm" name="fieldrequired"
+        inline @update:modelValue="$emit('update:modelValue', { ...modelValue, required: $event })"></f-switch>
 </template>
 
 <script setup>
 const props = defineProps({
-    modelValue: {
-        validator: (v) => v === true || v === false,
-    },
+    modelValue: {},
 });
 
 const emit = defineEmits(['update:modelValue']);
