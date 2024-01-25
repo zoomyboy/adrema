5e0dd8df6219185e007ddb89a3717f377856b441
philipp lang <philipp@aweos.de>
2024-01-25 23:14:12 +0100

    Fix toast response error

--- resources/js/lib/toast.js
+++ resources/js/lib/toast.js
@@ -6,7 +6,7 @@ var interceptor = [
         return config;
     },
     (err) => {
-        if (err.response.status === 422) {
+        if (err.response && err.response.status === 422) {
             var errors = err.response.data.errors;
             for (const error in errors) {
                 errors[error].forEach((errorMessage) => toast.error(errorMessage));
