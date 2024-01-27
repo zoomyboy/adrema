26d0f49568064f9f690bb2d86c8509abcfb97dba
philipp lang <philipp@aweos.de>
2024-01-27 21:31:06 +0100

    Lint

--- resources/js/views/contribution/VIndex.vue
+++ resources/js/views/contribution/VIndex.vue
@@ -1,31 +1,49 @@
 <template>
     <page-layout>
         <form target="_BLANK" class="max-w-4xl w-full mx-auto gap-6 grid-cols-2 grid p-6">
-            <f-text id="eventName" v-model="values.eventName" name="eventName" class="col-span-2"
-                label="Veranstaltungs-Name" required></f-text>
+            <f-text id="eventName" v-model="values.eventName" name="eventName" class="col-span-2" label="Veranstaltungs-Name" required></f-text>
             <f-text id="dateFrom" v-model="values.dateFrom" name="dateFrom" type="date" label="Datum von" required></f-text>
-            <f-text id="dateUntil" v-model="values.dateUntil" name="dateUntil" type="date" label="Datum bis"
-                required></f-text>
+            <f-text id="dateUntil" v-model="values.dateUntil" name="dateUntil" type="date" label="Datum bis" required></f-text>
 
             <f-text id="zipLocation" v-model="values.zipLocation" name="zipLocation" label="PLZ / Ort" required></f-text>
-            <f-select id="country" v-model="values.country" :options="countries" name="country" label="Land"
-                required></f-select>
+            <f-select id="country" v-model="values.country" :options="countries" name="country" label="Land" required></f-select>
 
             <div class="border-gray-200 shadow shadow-primary-700 p-3 shadow-[0_0_4px_gray] col-span-2">
-                <f-text id="search_text" ref="search_text_field" v-model="searchText" class="col-span-2" name="search_text"
-                    label="Suchen …" size="sm" @keypress.enter.prevent="onSubmitFirstMemberResult"></f-text>
+                <f-text
+                    id="search_text"
+                    ref="search_text_field"
+                    v-model="searchText"
+                    class="col-span-2"
+                    name="search_text"
+                    label="Suchen …"
+                    size="sm"
+                    @keypress.enter.prevent="onSubmitFirstMemberResult"
+                ></f-text>
                 <div class="mt-2 grid grid-cols-[repeat(auto-fill,minmax(180px,1fr))] gap-2 col-span-2">
-                    <f-switch v-for="member in search.results" :id="`members-${member.id}`" :key="member.id"
-                        v-model="values.members" :label="`${member.firstname} ${member.lastname}`" name="members[]"
-                        :value="member.id" size="sm" inline
-                        @keypress.enter.prevent="onSubmitMemberResult(member)"></f-switch>
+                    <f-switch
+                        v-for="member in search.results"
+                        :id="`members-${member.id}`"
+                        :key="member.id"
+                        v-model="values.members"
+                        :label="`${member.firstname} ${member.lastname}`"
+                        name="members[]"
+                        :value="member.id"
+                        size="sm"
+                        inline
+                        @keypress.enter.prevent="onSubmitMemberResult(member)"
+                    ></f-switch>
                 </div>
             </div>
 
-            <button v-for="(compiler, index) in compilers" class="btn btn-primary mt-3 inline-block" @click.prevent="
-                values.type = compiler.class;
-            submit();
-                                                " v-text="compiler.title"></button>
+            <button
+                v-for="(compiler, index) in compilers"
+                class="btn btn-primary mt-3 inline-block"
+                @click.prevent="
+                    values.type = compiler.class;
+                    submit();
+                "
+                v-text="compiler.title"
+            ></button>
         </form>
     </page-layout>
 </template>
@@ -75,7 +93,7 @@ export default {
                                 hasFullAddress: true,
                             },
                         },
-                        { headers: { 'X-Meta': 'false' } }
+                        {headers: {'X-Meta': 'false'}}
                     );
 
                     this.search.results = response.data.data;
