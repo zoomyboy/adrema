ddb44d64d606a460e46ffd01322243e536cdec47
philipp lang <philipp@aweos.de>
2024-02-04 02:51:10 +0100

    Add adrema_base_url

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
         <meta name="socketport" content="{{env('SOCKET_PORT')}}" />
+        <meta name="adrema_base_url" content="/">
         @if(auth()->id())
         <meta name="meilisearch_key" content="{{config('scout.meilisearch.key')}}" />
         @endif
