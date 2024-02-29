eaa880127da968daadfeb336d8ebe46a6b3880ae
philipp lang <philipp@aweos.de>
2024-02-29 20:36:02 +0100

    Add fresh Login for Remote login

--- app/Remote/Actions/LoginAction.php
+++ app/Remote/Actions/LoginAction.php
@@ -6,7 +6,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Crypt;
 use Lorisleiva\Actions\ActionRequest;
 use Lorisleiva\Actions\Concerns\AsAction;
-use Zoomyboy\LaravelNami\Authentication\Auth;
+use Zoomyboy\LaravelNami\Nami;
 
 class LoginAction
 {
@@ -26,7 +26,7 @@ class LoginAction
 
     public function handle(ActionRequest $request): JsonResponse
     {
-        Auth::login($request->mglnr, $request->password);
+        Nami::freshLogin($request->mglnr, $request->password);
 
         return response()->json([
             'access_token' => Crypt::encryptString(json_encode(['mglnr' => $request->mglnr, 'password' => $request->password])),
