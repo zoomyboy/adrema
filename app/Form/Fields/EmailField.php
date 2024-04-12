93d4511ee8149be75727c7683871ac5b81927f12
philipp lang <philipp@aweos.de>
2024-04-12 15:57:53 +0200

    Add Email field

---
+++ app/Form/Fields/EmailField.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Form\Fields;
+
+use App\Form\Models\Form;
+
+class EmailField extends TextField
+{
+
+    public static function name(): string
+    {
+        return 'E-Mail-Adresse';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getRegistrationRules(Form $form): array
+    {
+        return [$this->key => $this->required ? ['required', 'string', 'email'] : ['nullable', 'string']];
+    }
+}
