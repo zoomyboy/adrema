2691894b78f9497cace8a0729e150d18f058d866
philipp lang <philipp@aweos.de>
2024-04-17 10:53:59 +0200

    Fixed tests

--- database/migrations/2024_04_16_233540_add_allowcustom_field.php
+++ database/migrations/2024_04_16_233540_add_allowcustom_field.php
@@ -1,10 +1,8 @@
 <?php
 
-use App\Form\Models\Form;
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
@@ -18,7 +16,9 @@ return new class extends Migration
         foreach (DB::table('forms')->get() as $event) {
             $config = json_decode($event->config);
             $config->sections = array_map(function ($section) {
-                $section->fields = collect($section->fields)->map(function ($field) {
+                /** @var Collection<int, mixed> */
+                $fields = $section->fields;
+                $section->fields = collect($fields)->map(function ($field) {
                     if ($field->type === 'DropdownField' || $field->type === 'RadioField') {
                         $field->allowcustom = false;
                     }
@@ -33,7 +33,9 @@ return new class extends Migration
         foreach (DB::table('formtemplates')->get() as $event) {
             $config = json_decode($event->config);
             $config->sections = array_map(function ($section) {
-                $section->fields = collect($section->fields)->map(function ($field) {
+                /** @var Collection<int, mixed> */
+                $fields = $section->fields;
+                $section->fields = collect($fields)->map(function ($field) {
                     if ($field->type === 'DropdownField' || $field->type === 'RadioField') {
                         $field->allowcustom = false;
                     }
