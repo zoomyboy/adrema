35a8c59055c8446075241b039b930cd799592220
philipp lang <philipp@aweos.de>
2024-05-27 18:53:44 +0200

    Add private columns

---
+++ database/migrations/2024_05_27_180922_create_forms_is_active_column.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('forms', function (Blueprint $table) {
+            $table->boolean('is_active')->default(true)->after('name');
+            $table->boolean('is_private')->default(false)->after('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('forms', function (Blueprint $table) {
+            $table->dropColumn('is_active');
+            $table->dropColumn('is_private');
+        });
+    }
+};
