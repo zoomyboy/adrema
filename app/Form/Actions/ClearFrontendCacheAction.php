da02a76e2112e1b17f41e92e1171892f965f912d
philipp lang <philipp@aweos.de>
2024-06-10 11:16:28 +0200

    Lint

--- app/Form/Actions/ClearFrontendCacheAction.php
+++ app/Form/Actions/ClearFrontendCacheAction.php
@@ -10,7 +10,7 @@ class ClearFrontendCacheAction
 {
     use AsAction;
 
-    public function handle()
+    public function handle(): void
     {
         Http::get(app(FormSettings::class)->clearCacheUrl);
     }
