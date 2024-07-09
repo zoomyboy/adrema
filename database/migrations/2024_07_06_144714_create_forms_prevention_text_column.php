94597e57c1276b52fb1c0123077b96ea5d86b445
philipp lang <philipp@aweos.de>
2024-07-10 00:18:18 +0200

    Fix migration

--- database/migrations/2024_07_06_144714_create_forms_prevention_text_column.php
+++ database/migrations/2024_07_06_144714_create_forms_prevention_text_column.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Tests\RequestFactories\EditorRequestFactory;
 
 return new class extends Migration
 {
@@ -15,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('forms', function (Blueprint $table) {
-            $table->json('prevention_text')->after('description')->default(json_encode(EditorRequestFactory::new()->empty()->create()));
+            $table->json('prevention_text')->after('description')->default(json_encode(['time' => 4, 'blocks' => [], 'version' => 1.0]));
         });
     }
 
