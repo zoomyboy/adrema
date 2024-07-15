5fcee5f2845f80712f6e688af22d3ca4c0709ec3
philipp lang <philipp@aweos.de>
2024-07-15 23:49:28 +0200

    Move Creation of excel document to own action

---
+++ app/Form/Actions/CreateExcelDocumentAction.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Form\Actions;
+
+use App\Form\Models\Form;
+use App\Form\Models\Participant;
+use Illuminate\Database\Eloquent\Collection;
+use Lorisleiva\Actions\Concerns\AsAction;
+use Zoomyboy\TableDocument\SheetData;
+use Zoomyboy\TableDocument\TableDocumentData;
+
+class CreateExcelDocumentAction
+{
+    use AsAction;
+
+    public Form $form;
+
+    public function handle(Form $form, Collection $participants): string
+    {
+        $this->form = $form;
+
+        return file_get_contents($this->allSheet($participants)->compile($this->tempPath()));
+    }
+
+    /**
+     * @param Collection<int, Participant> $participants
+     */
+    private function allSheet(Collection $participants): TableDocumentData
+    {
+        $document = TableDocumentData::from(['title' => 'Anmeldungen für ' . $this->form->name, 'sheets' => []]);
+        $headers = $this->form->getFields()->map(fn ($field) => $field->name)->toArray();
+
+        $document->addSheet(SheetData::from([
+            'header' => $headers,
+            'data' => $participants
+                ->map(fn ($participant) => $this->form->getFields()->map(fn ($field) => $participant->getFields()->find($field)->presentRaw())->toArray())
+                ->toArray(),
+            'name' => 'Alle',
+        ]));
+
+        if ($this->form->export->groupBy) {
+            $groups = $participants->groupBy(fn ($participant) => $participant->getFields()->findByKey($this->form->export->groupBy)->presentRaw());
+
+            foreach ($groups as $name => $participants) {
+                $document->addSheet(SheetData::from([
+                    'header' => $headers,
+                    'data' => $participants
+                        ->map(fn ($participant) => $this->form->getFields()->map(fn ($field) => $participant->getFields()->find($field)->presentRaw())->toArray())
+                        ->toArray(),
+                    'name' => $name,
+                ]));
+            }
+
+            $document->addSheet(SheetData::from([
+                'header' => ['Wert', 'Anzahl'],
+                'data' => $groups->map(fn ($participants, $name) => [$name, (string) count($participants)])->toArray(),
+                'name' => 'Statistik',
+            ]));
+        }
+
+        return $document;
+    }
+
+    private function tempPath(): string
+    {
+        return sys_get_temp_dir() . '/' . str()->uuid()->toString();
+    }
+}
