95a466ff22910ada8be15dc9a8aa5e203fdb315d
philipp lang <philipp@aweos.de>
2025-05-29 23:17:10 +0200

    Move FilterSidebar to ui components

--- resources/js/components/page/Filter.vue
+++ resources/js/components/page/Filter.vue
@@ -1,7 +1,7 @@
 <template>
-    <page-filter-sidebar v-model="visible">
+    <ui-filter-sidebar v-model="visible">
         <slot name="fields"></slot>
-    </page-filter-sidebar>
+    </ui-filter-sidebar>
     <div class="px-6 py-2 border-b border-gray-600 items-center space-x-3">
         <div class="flex flex-col sm:flex-row items-stretch sm:items-end space-y-1 sm:space-y-0 sm:space-x-3">
             <slot name="buttons"></slot>
