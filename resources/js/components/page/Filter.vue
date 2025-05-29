e047b9a4f28388fbb0a10e31da822dddd8c16094
philipp lang <philipp@aweos.de>
2025-05-29 23:16:06 +0200

    Extract FilterSidebar component

--- resources/js/components/page/Filter.vue
+++ resources/js/components/page/Filter.vue
@@ -1,10 +1,7 @@
 <template>
-    <ui-sidebar :max="0" v-if="visible === true" @close="visible = false">
-        <page-header title="Filter" @close="visible = false"> </page-header>
-        <div class="grid gap-3 p-6">
-            <slot name="fields"></slot>
-        </div>
-    </ui-sidebar>
+    <page-filter-sidebar v-model="visible">
+        <slot name="fields"></slot>
+    </page-filter-sidebar>
     <div class="px-6 py-2 border-b border-gray-600 items-center space-x-3">
         <div class="flex flex-col sm:flex-row items-stretch sm:items-end space-y-1 sm:space-y-0 sm:space-x-3">
             <slot name="buttons"></slot>
---
+++ resources/js/components/page/FilterSidebar.vue
@@ -0,0 +1,19 @@
+<template>
+    <ui-sidebar :max="0" v-if="modelValue === true" @close="$emit('update:modelValue', false)">
+        <page-header title="Filter" @close="$emit('update:modelValue', false)"> </page-header>
+        <div class="grid gap-3 p-6">
+            <slot></slot>
+        </div>
+    </ui-sidebar>
+</template>
+
+<script setup>
+defineEmits(['update:modelValue']);
+
+defineProps({
+    modelValue: {
+        type: Boolean,
+        required: true,
+    },
+});
+</script>
