4cd18e9b3c4872355107622b469a1d45b317357b
philipp lang <philipp@aweos.de>
2025-06-11 18:29:47 +0200

    Lint

--- resources/js/views/member/ShowView.vue
+++ resources/js/views/member/ShowView.vue
@@ -6,46 +6,46 @@
         </template>
         <div class="p-3 grid gap-3 this-grid grow">
             <ui-box heading="Stammdaten" class="area-stamm hidden xl:block">
-                <stamm :inner="inner"></stamm>
+                <stamm :inner="inner" />
             </ui-box>
             <ui-box heading="Kontakt" class="area-kontakt hidden xl:block">
-                <kontakt :inner="inner"></kontakt>
+                <kontakt :inner="inner" />
             </ui-box>
             <ui-box class="area-stammkontakt block xl:hidden">
                 <tabs v-model="tabs.stammkontakt">
-                    <stamm v-show="tabs.stammkontakt.active === 'stamm'" :inner="inner"></stamm>
-                    <kontakt v-show="tabs.stammkontakt.active === 'kontakt'" :inner="inner"></kontakt>
+                    <stamm v-show="tabs.stammkontakt.active === 'stamm'" :inner="inner" />
+                    <kontakt v-show="tabs.stammkontakt.active === 'kontakt'" :inner="inner" />
                 </tabs>
             </ui-box>
 
             <ui-box container-class="" heading="Prävention" class="area-praev hidden xl:block">
-                <prae :inner="inner"></prae>
+                <prae :inner="inner" />
             </ui-box>
             <ui-box heading="System" class="area-system hidden xl:block">
-                <system :inner="inner"></system>
+                <system :inner="inner" />
             </ui-box>
             <ui-box class="area-praesystem block xl:hidden">
                 <tabs v-model="tabs.praesystem">
-                    <prae v-show="tabs.praesystem.active === 'prae'" :inner="inner"></prae>
-                    <system v-show="tabs.praesystem.active === 'system'" :inner="inner"></system>
+                    <prae v-show="tabs.praesystem.active === 'prae'" :inner="inner" />
+                    <system v-show="tabs.praesystem.active === 'system'" :inner="inner" />
                 </tabs>
             </ui-box>
 
             <ui-box class="area-membershipcourse hidden xl:block">
                 <tabs v-model="tabs.membershipcourse">
-                    <courses v-show="tabs.membershipcourse.active === 'course'" :value="inner.courses"></courses>
-                    <memberships v-show="tabs.membershipcourse.active === 'membership'" :value="inner.memberships"> </memberships>
+                    <courses v-show="tabs.membershipcourse.active === 'course'" :value="inner.courses" />
+                    <memberships v-show="tabs.membershipcourse.active === 'membership'" :value="inner.memberships" />
                 </tabs>
             </ui-box>
             <ui-box heading="Ausbildungen" class="area-courses xl:hidden">
-                <courses :value="inner.courses"></courses>
+                <courses :value="inner.courses" />
             </ui-box>
             <ui-box heading="Mitgliedschaften" class="area-memberships xl:hidden">
-                <memberships :value="inner.memberships"></memberships>
+                <memberships :value="inner.memberships" />
             </ui-box>
 
             <ui-box heading="Zahlungen" class="area-payments">
-                <payments :value="inner.invoicePositions"></payments>
+                <payments :value="inner.invoicePositions" />
             </ui-box>
 
             <ui-box heading="Karte" container-class="grow" class="area-map hidden xl:flex">
