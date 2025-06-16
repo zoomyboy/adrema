d0bd5bb0de231e7b80ffbe430249059293014129
philipp lang <philipp@aweos.de>
2025-06-17 01:50:45 +0200

    Lint

--- resources/js/components/ui/Button.vue
+++ resources/js/components/ui/Button.vue
@@ -1,24 +1,22 @@
 <template>
     <button v-bind="$attrs" class="btn btn-primary relative group">
         <div :class="{hidden: !isLoading, flex: isLoading}" class="absolute items-center top-0 h-full left-0 ml-2">
-            <ui-spinner class="border-primary-400 w-6 h-6 group-hover:border-primary-200"></ui-spinner>
+            <ui-spinner class="border-primary-400 w-6 h-6 group-hover:border-primary-200" />
         </div>
-        <slot></slot>
+        <slot />
     </button>
 </template>
 
 <script>
-import {menuStore} from '../../stores/menuStore.js';
-
 export default {
-    data: function () {
-        return {};
-    },
     props: {
         isLoading: {
             type: Boolean,
             default: false,
         },
+    },
+    data: function () {
+        return {};
     },
 };
 </script>
