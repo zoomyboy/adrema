b86fe9634f70140dcbf241f1c04a7bfda40d057b
philipp lang <philipp@aweos.de>
2025-06-17 01:44:55 +0200

    Lint

--- resources/js/components/ui/Popup.vue
+++ resources/js/components/ui/Popup.vue
@@ -1,23 +1,22 @@
 <template>
     <div class="fixed z-40 top-0 left-0 w-full h-full flex items-center justify-center p-6 bg-black/60 backdrop-blur-sm">
-        <div
-            class="relative rounded-lg p-8 bg-zinc-800 shadow-2xl shadow-black border border-zinc-700 border-solid w-full max-h-full flex flex-col overflow-auto"
-            :class="full ? 'h-full' : innerWidth"
+        <div class="relative rounded-lg p-8 bg-zinc-800 shadow-2xl shadow-black border border-zinc-700 border-solid w-full max-h-full flex flex-col overflow-auto"
+             :class="full ? 'h-full' : innerWidth"
         >
             <div class="absolute top-0 right-0 mt-6 mr-6 flex space-x-6">
-                <slot name="actions"></slot>
+                <slot name="actions" />
                 <a href="#" @click.prevent="$emit('close')">
-                    <ui-sprite src="close" class="text-zinc-400 w-6 h-6"></ui-sprite>
+                    <ui-sprite src="close" class="text-zinc-400 w-6 h-6" />
                 </a>
             </div>
-            <h3 v-if="heading" class="font-semibold text-primary-200 text-xl" v-html="heading"></h3>
+            <h3 v-if="heading" class="font-semibold text-primary-200 text-xl" v-html="heading" />
             <div class="text-primary-100 group is-popup grow flex flex-col">
                 <suspense>
                     <div>
-                        <slot></slot>
+                        <slot />
                     </div>
                     <template #fallback>
-                        <ui-loading></ui-loading>
+                        <ui-loading />
                     </template>
                 </suspense>
             </div>
