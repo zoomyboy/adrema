17808c6eb19981ebea9b4b44b3526d8c78e056c5
philipp lang <philipp@aweos.de>
2025-07-06 02:06:35 +0200

    Add special-types to form

--- app/Form/Enums/SpecialType.php
+++ app/Form/Enums/SpecialType.php
@@ -7,6 +7,12 @@ enum SpecialType: string
     case FIRSTNAME = 'Vorname';
     case LASTNAME = 'Nachname';
     case EMAIL = 'E-Mail-Adresse';
+    case BIRTHDAY = 'Geburtsdatum';
+    case ZIP = 'PLZ';
+    case LOCATION = 'Ort';
+    case ADDRESS = 'Adresse';
+    case GENDER = 'Geschlecht';
+    case LEADER = 'LeiterIn';
 
     /**
      * @return array<int, array{name: string, id: string}>
